f75f6ce380d2347f0e752acfb659d8f4b83fd707
edin <edin.avdic29@gmail.com>
2025-03-21 14:16:06 +0100

    match class defined

--- src/main/java/com/edinavdic/Match.java
+++ src/main/java/com/edinavdic/Match.java
@@ -1,4 +1,45 @@
 package com.edinavdic;
 
 public class Match {
+
+    private final String homeTeam;
+    private final String awayTeam;
+    private int homeTeamScore;
+    private int awayTeamScore;
+
+    public Match(String homeTeam, String awayTeam) {
+        if (homeTeam == null || homeTeam.isEmpty() || awayTeam == null || awayTeam.isEmpty()) {
+            throw new IllegalArgumentException("HomeTeam and AwayTeam names cannot be null or empty");
+        }
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeTeamScore = 0;
+        this.awayTeamScore = 0;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeTeamScore() {
+        return homeTeamScore;
+    }
+
+    public int getAwayTeamScore() {
+        return awayTeamScore;
+    }
+
+    public void updateScore(int homeTeamScore, int awayTeamScore) { // sync
+        this.homeTeamScore = homeTeamScore;
+        this.awayTeamScore = awayTeamScore;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d - %s %d", homeTeam, homeTeamScore, awayTeam, awayTeamScore);
+    }
 }
