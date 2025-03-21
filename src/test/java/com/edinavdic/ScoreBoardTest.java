2a8bafb14e8d97ce1b0e43583604e020a43e2b3e
edin <edin.avdic29@gmail.com>
2025-03-21 14:01:28 +0100

    tests name update

--- src/test/java/com/edinavdic/ScoreBoardTest.java
+++ src/test/java/com/edinavdic/ScoreBoardTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ScoreBoardTest {
 
     @Test
-    void startMatchAndVerifyMatchExists() {
+    void startMatch_ValidInput_MatchIsAdded() {
         ScoreBoard sb = new ScoreBoard();
         sb.startMatch("Denmark", "Norway");
         assertEquals(1, sb.getSummary().size());
     }
 
     @Test
-    void startTwoMatchesAndVerifyMatchesExist() {
+    void startTwoMatches_MatchesAreAddedToScoreboard() {
         ScoreBoard sb = new ScoreBoard();
         sb.startMatch("Denmark", "Norway");
         sb.startMatch("Sweden", "Finland");
@@ -22,7 +22,7 @@ public class ScoreBoardTest {
     }
 
     @Test
-    void startMatchAndVerifyMatchUpdatesCorrectly() {
+    void startMatch_UpdateScore_ScoreIsUpdatedCorrectly() {
         ScoreBoard sb = new ScoreBoard();
         sb.startMatch("Denmark", "Norway");
         sb.updateScore("Denmark", "Norway", 0, 2);
@@ -30,10 +30,22 @@ public class ScoreBoardTest {
     }
 
     @Test
-    void finishMatchAndVerifyMatchIsRemoved() {
+    void finishMatch_MatchExists_MatchIsRemoved() {
         ScoreBoard sb = new ScoreBoard();
         sb.startMatch("Denmark", "Norway");
         sb.finishMatch("Denmark", "Norway");
         assertEquals(0, sb.getSummary().size());
+    }
+
+    @Test
+    void startMatch_InvalidHomeTeam_ThrowsException() {
+        ScoreBoard sb = new ScoreBoard();
+        assertThrows(IllegalArgumentException.class, () -> sb.startMatch("", "Norway"));
+    }
+
+    @Test
+    void startMatch_InvalidAwayTeam_ThrowsException() {
+        ScoreBoard sb = new ScoreBoard();
+        assertThrows(IllegalArgumentException.class, () -> sb.startMatch("Denmark", null));
     }
 }
